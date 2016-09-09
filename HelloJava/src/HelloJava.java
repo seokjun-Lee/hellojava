7f09a0774fb07dcc889ffccc134ad5a75b88f0f1
soft <soft@soft-PC>
2016-09-09 11:17:45 +0900

    edit

--- HelloJava/src/HelloJava.java
+++ HelloJava/src/HelloJava.java
@@ -4,6 +4,7 @@ class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloJava!! 2013041009 ¿ÃºÆ¡ÿ");
+		System.out.println("HelloJava!!");
 	}
 
 }

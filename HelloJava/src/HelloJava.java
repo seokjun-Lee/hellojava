47260519cca7e471e244531c2e4a4a6b4dd26003
soft <soft@soft-PC>
2016-09-09 11:15:26 +0900

    create project

--- HelloJava/src/HelloJava.java
+++ HelloJava/src/HelloJava.java
@@ -3,7 +3,7 @@ class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloJava");
+		System.out.println("HelloJava!! 2013041009 ¿ÃºÆ¡ÿ");
 	}
 
 }
